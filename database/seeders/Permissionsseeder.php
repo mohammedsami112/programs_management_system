6586972892e762b6b357a86ca088e170019b2428
Mohammed Sami <mohmaedsamy112@gmail.com>
2023-10-24 10:49:04 +0300

    Create Seeders

---
+++ database/seeders/Permissionsseeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Permissionsseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Permission::create([
+            'title' => 'Admin',
+            'permissions' => 'users_view,users_restore,users_create,users_update,users_delete,permissions_view,permissions_create,permissions_update,permissions_delete,programs_view,programs_create,programs_update,programs_delete,programs_add_users,programs_delete_users,logs_view,programs_restore,permissions_restore,logs_all'
+        ]);
+    }
+}
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'username' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+            'country' => 'country',
+            'city' => 'city',
+            'permission' => 1,
+        ]);
+    }
+}
